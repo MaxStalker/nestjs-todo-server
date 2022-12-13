d06ecf01297bee83d538bc5908b6f3b8cf6187cc
Max Daunarovich <max.daunarovich@gmail.com>
2022-12-13 22:51:51 +0300

    Initial commit

---
+++ src/database/database.providers.ts
@@ -0,0 +1,10 @@
+import * as mongoose from 'mongoose';
+
+export const name = 'DATABASE_CONNECTION';
+export const databaseProviders = [
+  {
+    provide: name,
+    useFactory: (): Promise<typeof mongoose> =>
+      mongoose.connect('mongodb://localhost'),
+  },
+];
